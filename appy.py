ba5b5c5f768ee35ed62a73532ed1ad54d1d07d7a
Kellyy621 <yekelly621@gmail.com>
2025-03-05 10:22:58 -0500

    help

--- appy.py
+++ appy.py
@@ -1,10 +1,12 @@
-User_input = int(input("number"))
-for i in range (1, 11):
-          if  User_input > 0:
-           for i in range(1, 11):
-               x = User_input*i
-               print (x)
-if User_input == 0:
-        print ("error, pick another number")
-if User_input == ("exit"):
-     print("Game finished!")
+
+
+while True:
+        User_input = int(input("number"))
+        for i in range (1, 11):
+                x = User_input*i
+                print (x)
+        if (int(User_input)) == 0:
+                print ("error, pick another number")
+        c = input("number")
+        if c == ("exit"):
+                break 
